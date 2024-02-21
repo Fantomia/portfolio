5b667595fb02122e1abf1664f8d5dfa634c07248
Fantomia <jonathan.andri@gmail.com>
2024-02-21 16:47:10 +0100

    Ajout de la page login :
    Création d'un fichier login.html et login.js
    
    Ajout des fonctions de récupération des données API /POST/users/login
    
    Ajout des fonctions de récupération du token

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,59 @@
+const loginIndex = document.querySelector(".loginIndex")
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Vérifie si le token est déjà présent dans le localStorage
+    const storedToken = localStorage.getItem('token');
+    const loginLogoutLink = document.getElementById('loginLogout');
+
+    if (storedToken) {
+        // Si le token est présent, change le texte du lien à "logout"
+        loginLogoutLink.textContent = 'logout';
+
+        // Ajoute un gestionnaire d'événements pour gérer le "logout"
+        loginLogoutLink.addEventListener('click', function () {
+            // Supprime le token du localStorage
+            localStorage.removeItem('token');
+            loginLogoutLink.textContent = 'login';
+            alert('Vous êtes maintenant déconnecté');
+        });
+    }
+});
+
+    const loginForm = document.querySelector('.login form');
+  
+    loginForm.addEventListener('submit', async function (event) {
+      event.preventDefault(); // Désactivation du comportement par défaut du navigateur
+  
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      // Créez l'objet avec les données pour l'API
+      const loginData = {
+        email: email,
+        password: password
+      };
+  
+      try {
+        const response = await fetch('http://localhost:5678/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(loginData)
+        });
+  
+        const responseData = await response.json();
+        console.log(responseData)
+  
+        // Vérifiez si la connexion a réussi ou échoué
+        if (response.ok) {
+          localStorage.setItem('token', responseData.token);
+          window.location.href = 'index.html';
+        } else {
+          console.log('Échec de la connexion:', responseData);
+          alert("Email ou Mot de passe incorrect");
+        }
+      } catch (error) {
+        console.error('Erreur lors de la requête POST:', error);
+      }
+    });
